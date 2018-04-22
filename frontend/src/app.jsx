292935717df134b6626a164b9220f15300a24efa
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-22 13:36:12 +0200

    feat(frontend): add Showing component to show theatre floorplan

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,38 @@
 import React from 'react'
+import classnames from 'classnames'
 import './base.styl'
+import './showing.styl'
 
-export default () =>
-  <h1>Minimal React App booted!</h1>
+const floorPlan = [
+  '  ffffffffff  ',
+  ' ffffffffffff ',
+  'ffffffffffffff',
+  ' ffffffffffff ',
+  '  ffffffffff  ',
+]
+
+
+const Seat = ({ type }) => (
+  <div className={classnames(
+      'seat',
+      { 'seat-free': type === 'f' },
+      { 'seat-placeholder': type === ' ' },
+    )}
+  />
+)
+
+const Row = ({ seats }) => (
+  <div className="row">
+    {[...seats].map(seatType => <Seat type={seatType} />)}
+  </div>
+)
+
+const Showing = () => (
+  <div className="showing">
+    <h1>Cinema Ticket System</h1>
+    <p>Choose your seats</p>
+    {floorPlan.map(seats => <Row seats={seats} />)}
+  </div>
+)
+
+export default () => <Showing />
