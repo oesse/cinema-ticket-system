e29136585ba19a53f3f7ce9d958cbf4b15ee60e2
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-22 19:45:59 +0200

    test: fix e2e test after change in signature

--- backend/test/rest-api.e2e.js
+++ backend/test/rest-api.e2e.js
@@ -4,10 +4,11 @@ import request from 'supertest'
 
 import api from '../src/rest-api'
 
+const userId = 'userId'
 describe('GET /floorplan', () => {
   it('returns the floorplan of a theatre', async () => {
     const { body } = await request(api)
-      .get('/floorplan')
+      .get(`/floorplan?userId=${userId}`)
       .expect(OK)
     expect(body).to.be.an('array').which.has.length.above(1)
   })
@@ -17,8 +18,9 @@ describe('POST /reserve-seat', () => {
   it('sets the given seat to reserved and returns the new floorplan', async () => {
     const { body } = await request(api)
       .post('/reserve-seat')
-      .send({ row: 1, number: 1 })
+      .send({ row: 1, number: 1, userId })
       .expect(OK)
+
     expect(body).to.be.an('array').which.has.length.above(1)
     const [firstRow] = body
     const seat = [...firstRow].find(seatOrPlaceholder => seatOrPlaceholder !== ' ')
@@ -28,16 +30,16 @@ describe('POST /reserve-seat', () => {
   it('persists reservations for for multiple reservations', async () => {
     await request(api)
       .post('/reserve-seat')
-      .send({ row: 1, number: 1 })
+      .send({ row: 1, number: 1, userId })
       .expect(OK)
 
     await request(api)
       .post('/reserve-seat')
-      .send({ row: 1, number: 3 })
+      .send({ row: 1, number: 3, userId })
       .expect(OK)
 
     const { body } = await request(api)
-      .get('/floorplan')
+      .get(`/floorplan?userId=${userId}`)
       .expect(OK)
 
     const [firstRow] = body
@@ -49,14 +51,14 @@ describe('POST /cancel-seat', () => {
   beforeEach('reserve seat', async () => {
     await request(api)
       .post('/reserve-seat')
-      .send({ row: 1, number: 1 })
+      .send({ row: 1, number: 1, userId })
       .expect(OK)
   })
 
   it('clears the reservation for the given seat', async () => {
     const { body } = await request(api)
       .post('/cancel-seat')
-      .send({ row: 1, number: 1 })
+      .send({ row: 1, number: 1, userId })
       .expect(OK)
 
     const [firstRow] = body
