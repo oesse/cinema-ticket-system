18c731c71467653a18cfe1ae7a03b06756b82184
Oliver Esser <oliver.esser@campus.tu-berlin.de>
2018-04-22 18:55:39 +0200

    feat: keep reference when nothing expired

--- backend/src/remove-expired-reservations.js
+++ backend/src/remove-expired-reservations.js
@@ -3,7 +3,13 @@ function isExpired(date, timeLimitInMinutes, now) {
 }
 
 export default function removeExpiredReservations(reservations, timeLimitInMinutes, now) {
-  return Object.entries(reservations)
+  const filteredEntries = Object.entries(reservations)
     .filter(([, value]) => isExpired(value.date, timeLimitInMinutes, now))
+
+  // Keep reference when nothing is filtered out
+  if (filteredEntries.length === Object.keys(reservations).length) {
+    return reservations
+  }
+  return filteredEntries
     .reduce((result, [key, value]) => ({ ...result, [key]: value }), {})
 }
--- backend/test/remove-expired-reservations.spec.js
+++ backend/test/remove-expired-reservations.spec.js
@@ -15,11 +15,7 @@ describe('removeExpiredReservations', () => {
       const now = LocalDateTime.of(2018, 4, 22, 18, 8, 0)
       const filteredReservations = removeExpiredReservations(reservations, timeLimitInMinutes, now)
 
-      expect(filteredReservations).to.eql({
-        1.3: { row: 1, number: 3, date: LocalDateTime.of(2018, 4, 22, 18, 0, 0) },
-        1.2: { row: 1, number: 2, date: LocalDateTime.of(2018, 4, 22, 17, 59, 0) },
-        1.5: { row: 1, number: 5, date: LocalDateTime.of(2018, 4, 22, 18, 5, 0) },
-      })
+      expect(filteredReservations).to.equal(reservations)
     })
   })
 
